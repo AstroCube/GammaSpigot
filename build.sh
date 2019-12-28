5daeb301608f557fd9829f688ed1af277315d618
TomateDeveloper <ianfe@martina.com.co>
2019-12-28 16:54:21 -0500

    Added build bridge to build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule update --init && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
+git submodule update --init && ./buildBridge.sh && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
