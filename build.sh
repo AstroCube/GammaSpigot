f235a15ff44850bc0b3921113c002198d9daf992
TomateDeveloper <ianfe@martina.com.co>
2019-12-29 13:44:30 -0500

    Updated offline install

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule update --init && ./buildWaterfall.sh && ./buildBridge.sh && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
+git submodule update --init && sh buildWaterfall.sh && sh buildBridge.sh && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
