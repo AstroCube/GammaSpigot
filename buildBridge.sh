9a17ed39cf9229b3186a9bf592d2bf97b453f5ee
TomateDeveloper <ianfe@martina.com.co>
2019-12-29 15:16:48 -0500

    Updated

--- buildBridge.sh
+++ buildBridge.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/SeocraftNetwork/Spigot-Bungee-Bridge
 cd Spigot-Bungee-Bridge
-mvn clean install:install-file -DgroupId=net.seocraft -DartifactId=waterfall-chat -Dversion=1.8 -Dpackaging=jar -Dfile=../Waterfall/Waterfall-Proxy/chat/target/waterfall-chat-1.8.jar
+mvn clean install:install-file -DgroupId=net.seocraft -DartifactId=waterfall-chat -Dversion=1.8 -Dpackaging=jar -Dfile=../Waterfall/Waterfall-Proxy/chat/target/waterfall-chat-1.8.jar && mvn install
