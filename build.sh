3c611693a2f759a9785c5b3c221c2047983bb0d9
TomateDeveloper <ianfe@martina.com.co>
2019-12-29 14:27:47 -0500

    Updated Spigot waterfall

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git submodule update --init && sh buildWaterfall.sh && sh buildBridge.sh && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install
+git submodule update --init && sh buildWaterfall.sh && sh buildBridge.sh && ./remap.sh && ./decompile.sh && ./init.sh && ./newApplyPatches.sh && mvn clean install:install-file -DgroupId=me.fixeddev -DartifactId=Spigot-Bungee-Bridge -Dversion=1.8-SNAPSHOT -Dpackaging=jar -Dfile=../Spigot-Bungee-Bridge/chat/target/Spigot-BungeeBridge-1.8-SNAPSHOT.jar
